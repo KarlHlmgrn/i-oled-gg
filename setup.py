66227a1b505a9e2de2fd9bb94bef9e9c665b5e4c
KarlHlmgrn <karl.holmgren4@gmail.com>
2023-01-18 16:44:20 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="i-oled-gg",
+    version="1.0",
+    author="KarlHlmgrn",
+    author_email="karlhlmgrn.github@gmail.com",
+    description="Simple library to send images or GIFs to a SteelSeries keyboard OLED screen",
+    install_requires=[
+        "easyhid>=0.0.10",
+        "Pillow>=9.4.0"
+    ],
+    url="https://github.com/KarlHlmgrn/i-oled-gg",
+    packages=find_packages()
+)
